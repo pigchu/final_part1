80c36d8358a58a40790894b3841dd35326f2214b
pigchu <chu101166.cts08@nctu.edu.tw>
2021-06-02 00:45:05 +0800

    0602

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
-$(function () { $('#collapseFour').collapse({
-        toggle: false
-    })});
-$(function () { $('#collapseTwo').collapse('show')});
-$(function () { $('#collapseThree').collapse('toggle')});
-$(function () { $('#collapseOne').collapse('hide')});
+// 列表
+$(document).ready(function() {
+	$('.cart>li>a').click(function(event) {
+		event.preventDefault();
+		$(this).toggleClass('active');
+		$(this).siblings('ul').slideToggle(2000);
+		});
+	});
+// 小圖換大圖
+$(function(){	
+	$("#small img").click(function(){		
+		var N = $(this).attr("id").substr(2);	
+		$("#big").attr( "src" , "img/book" + N + ".jpg" );	
+	});
+});
+// 實時顯示上傳圖片
+$('#file').change(function() {
+  var file = $('#file')[0].files[0];
+  var reader = new FileReader;
+  reader.onload = function(e) {
+    $('#demo').attr('src', e.target.result);
+  };
+  reader.readAsDataURL(file);
+});
+//上傳成功
+$(function(){	
+	$("#send").click(function(){	
+	window.alert("上傳成功");
+	window.location.reload();
+	});
+});
